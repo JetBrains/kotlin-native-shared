273d64ca6f97d2a97495a0bdd9b48624c06acfa0
Alexander Gorshenev <alexander.gorshenev@jetbrains.com>
2017-07-06 12:39:02 +0300

    Moved clang flags out of the root build.gardle. Collected clang args in ClangArgs.kt Switched dependencies/build.gradle to KonanTarget. Introduced KonanProperties to take care of target specific properties.

---
+++ src/main/kotlin/org/jetbrains/kotlin/konan/target/Properties.kt
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2010-2017 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jetbrains.kotlin.konan.properties
+
+import org.jetbrains.kotlin.konan.file.*
+
+typealias Properties = java.util.Properties
+
+fun File.loadProperties(): Properties {
+    val properties = java.util.Properties()
+    this.bufferedReader().use { reader ->
+        properties.load(reader)
+    }
+    return properties
+}
+
+fun loadProperties(path: String): Properties = File(path).loadProperties()
+
+fun File.saveProperties(properties: Properties) {
+    this.outputStream().use { 
+        properties.store(it, null) 
+    }
+}
+
+fun Properties.saveToFile(file: File) = file.saveProperties(this)
+
+fun Properties.propertyString(key: String, suffix: String? = null): String?
+    = this.getProperty(key.suffix(suffix))
+
+/**
+ * TODO: this method working with suffixes should be replaced with
+ * functionality borrowed from def file parser and unified for interop tool
+ * and kotlin compiler.
+ */
+fun Properties.propertyList(key: String, suffix: String? = null): List<String> {
+    val value = this.getProperty(key.suffix(suffix)) ?: this.getProperty(key)
+    return value?.split(' ') ?: emptyList()
+}
+
+fun Properties.hasProperty(key: String, suffix: String? = null): Boolean
+    = this.getProperty(key.suffix(suffix)) != null
+
+fun String.suffix(suf: String?): String =
+    if (suf == null) this
+    else "${this}.$suf"
+
+
