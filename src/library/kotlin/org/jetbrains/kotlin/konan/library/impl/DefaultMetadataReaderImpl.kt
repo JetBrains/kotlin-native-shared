d45205907b0ac76c8f1b03a7eb9e263e16ebcb45
alexander-gorshenev <alexander.gorshenev@jetbrains.com>
2018-09-13 02:10:34 +0300

    Provided library versioning in klib. (#2038)
    * Provided library versioning in klib.
    * A test for library mismatch messages.

---
+++ src/library/kotlin/org/jetbrains/kotlin/konan/library/impl/DefaultMetadataReaderImpl.kt
@@ -0,0 +1,13 @@
+package org.jetbrains.kotlin.konan.library.impl
+
+import org.jetbrains.kotlin.konan.library.KonanLibraryLayout
+import org.jetbrains.kotlin.konan.library.MetadataReader
+
+internal object DefaultMetadataReaderImpl : MetadataReader {
+
+    override fun loadSerializedModule(libraryLayout: KonanLibraryLayout): ByteArray =
+            libraryLayout.moduleHeaderFile.readBytes()
+
+    override fun loadSerializedPackageFragment(libraryLayout: KonanLibraryLayout, fqName: String): ByteArray =
+            libraryLayout.packageFile(fqName).readBytes()
+}
