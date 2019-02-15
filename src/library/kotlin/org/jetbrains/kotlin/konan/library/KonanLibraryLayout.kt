kingj1261/kotlin-native

Whole module IR serialization/deserialization. With public descriptors inserted and wrapped ones used instead of the locals.

shared/src/library/kotlin/org/jetbrains/kotlin/konan/library/KonanLibraryLayout.kt
@@ -44,4 +44,15 @@ interface KonanLibraryLayout {
 
     fun packageFragmentFile(packageFqName: String, partName: String) =
             File(packageFragmentsDir(packageFqName), "$partName$KLIB_METADATA_FILE_EXTENSION_WITH_DOT")
+    val irDir
+        get() = File(libDir, "ir")
+    fun hiddenDeclarationFile(declarationId: String)
+        = File(irDir, "hidden_$declarationId.knd")
+    fun visibleDeclarationFile(declarationId: String)
+        = File(irDir, "visible_$declarationId.knd")
+    val wholeIrFile
+        get() = File(irDir, "wholeIr.kni")
+    val irIndex: File
+        get() = File(irDir, "debug.txt")
+
 }

shared/src/library/kotlin/org/jetbrains/kotlin/konan/library/impl/KonanLibraryImpl.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2010-2018 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.jetbrains.kotlin.konan.library.impl
 
 import org.jetbrains.kotlin.konan.file.File
@@ -8,7 +24,6 @@ import org.jetbrains.kotlin.konan.properties.propertyList
 import org.jetbrains.kotlin.konan.target.KonanTarget
 import org.jetbrains.kotlin.konan.util.defaultTargetSubstitutions
 import org.jetbrains.kotlin.konan.util.substitute
-
 
 class KonanLibraryImpl(
         override val libraryFile: File,
@@ -68,6 +83,10 @@ class KonanLibraryImpl(
                     require(it.size == fileList.size) { "Duplicated names: ${fileList.groupingBy { it }.eachCount().filter { (_, count) -> count > 1 }}" }
                 }
             }
+
+    override val wholeIr: ByteArray by lazy { layout.inPlace {  metadataReader.loadWholeIr(it) }}
+
+    override fun irDeclaration(index: Long, isLocal: Boolean) = layout.inPlace { metadataReader.loadIrDeclaraton(it, index, isLocal) }
 
     override fun toString() = "$libraryName[default=$isDefault]"
 }

JetBrains/kotlin-native-shared

src/library/kotlin/org/jetbrains/kotlin/konan/library/KonanLibraryLayout.kt
@@ -44,4 +44,15 @@ interface KonanLibraryLayout {
 
     fun packageFragmentFile(packageFqName: String, partName: String) =
             File(packageFragmentsDir(packageFqName), "$partName$KLIB_METADATA_FILE_EXTENSION_WITH_DOT")
+    val irDir
+        get() = File(libDir, "ir")
+    fun hiddenDeclarationFile(declarationId: String)
+        = File(irDir, "hidden_$declarationId.knd")
+    fun visibleDeclarationFile(declarationId: String)
+        = File(irDir, "visible_$declarationId.knd")
+    val wholeIrFile
+        get() = File(irDir, "irHeaders.kni")
+    val irIndex: File
+        get() = File(irDir, "uniqIdTableDump.txt")
+
 }

src/library/kotlin/org/jetbrains/kotlin/konan/library/impl/KonanLibraryImpl.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2010-2018 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.jetbrains.kotlin.konan.library.impl
 
 import org.jetbrains.kotlin.konan.file.File
@@ -8,7 +24,6 @@ import org.jetbrains.kotlin.konan.properties.propertyList
 import org.jetbrains.kotlin.konan.target.KonanTarget
 import org.jetbrains.kotlin.konan.util.defaultTargetSubstitutions
 import org.jetbrains.kotlin.konan.util.substitute
-
 
 class KonanLibraryImpl(
         override val libraryFile: File,
@@ -68,6 +83,10 @@ class KonanLibraryImpl(
                     require(it.size == fileList.size) { "Duplicated names: ${fileList.groupingBy { it }.eachCount().filter { (_, count) -> count > 1 }}" }
                 }
             }
+
+    override val irHeader: ByteArray by lazy { layout.inPlace {  metadataReader.loadWholeIr(it) }}
+
+    override fun irDeclaration(index: Long, isLocal: Boolean) = layout.inPlace { metadataReader.loadIrDeclaraton(it, index, isLocal) }
 
     override fun toString() = "$libraryName[default=$isDefault]"
 }